58be2027796414ddebe3e3a80b6fb1d61fa5edcc
Vahidone <v.ban.roma@gmail.com>
2023-10-30 19:10:55 +0100

    feat: add another axios call

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,61 @@ createApp({
       });
     },
 
+    addTodo() {
+      const todoFormData = {
+        //nome della chiave: valore della chiave
+        newTodoText: this.newTodoText,
+      }
 
+      axios.post(
+        this.apiUrl,
+        todoFormData,
+        { headers: { 'Content-Type': 'multipart/form-data' } }
+      ).then((response) => {
+        console.log(response.data);
+
+        this.getTodo();
+
+        this.newTodoText = '';
+
+      });
+    },
+
+    toggleTodo(index) {
+    // console.log(index);
+
+    const todoFormData = {
+      toggleTodoIndex: index,
+    }
+
+    axios.post(
+      this.apiUrl,
+      todoFormData,
+      //headers per ingannare il server e fargli credere che è un form
+      { headers: { 'Content-Type': 'multipart/form-data' } }
+    ).then((response) => {
+      // console.log(response.data);
+      this.getTodo(); //funzione che aggiorna i to do ricaricando la pagina
+    });
+    },
+
+    deleteTodo(index) {
+    const todoFormData = {
+      deleteTodoIndex: index,
+    }
+
+    axios.post(
+      this.apiUrl,
+      todoFormData,
+      //headers per ingannare il server e fargli credere che è un form
+      { headers: { 'Content-Type': 'multipart/form-data' } }
+    ).then((response) => {
+      // console.log(response.data);
+
+      this.getTodo(); //funzione che aggiorna i to do ricaricando la pagina
+    });
+    }
+  },
 
   mounted() {
   this.getTodo();
