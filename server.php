a19b597d2ebeca3c96faee1e1b50f10a7c353b92
Vahidone <v.ban.roma@gmail.com>
2023-10-30 19:05:28 +0100

    feat: update server functions

--- server.php
+++ server.php
@@ -8,3 +8,46 @@ $file_text = file_get_contents($file_url);
 
 //converto il testo json in un array php
 $todo_list = json_decode($file_text);
+
+if (isset($_POST['newTodoText'])) {
+
+  $newTodo = [
+      'text' => $_POST['newTodoText'],
+      'done' => false,
+  ];
+
+  array_push($todo_list, $newTodo);
+  // print_r($todo_list);
+  file_put_contents($file_url, json_encode($todo_list));
+} else if (isset($_POST['toggleTodoIndex'])) {
+  //togglo un todo
+  $todoIndex = $_POST['toggleTodoIndex'];
+
+  // echo $todo_list[$todoIndex]->text;
+
+  if ($todo_list[$todoIndex]->done == 1) {
+      //if done
+      $todo_list[$todoIndex]->done = false;
+  } else {
+      //if not done
+      $todo_list[$todoIndex]->done = true;
+  }
+  ;
+  file_put_contents($file_url, json_encode($todo_list));
+
+} else if (isset($_POST['deleteTodoIndex'])) {
+  //elimino il todo
+
+  $todoIndex = $_POST['deleteTodoIndex'];
+
+  array_splice($todo_list, $todoIndex, 1);
+
+  file_put_contents($file_url, json_encode($todo_list));
+
+
+} else {
+  //mostro la lista dei to do
+  
+  header('Content-Type: application/json');
+  echo json_encode($todo_list);
+}
