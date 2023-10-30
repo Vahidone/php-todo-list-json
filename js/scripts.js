96999761aeda6e2b053ae83f27a66c193787a6fd
Vahidone <v.ban.roma@gmail.com>
2023-10-30 19:08:46 +0100

    add: init js file structure

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      titol: 'Todo List',
+      apiUrl: './server.php',
+      todoList: [],
+      newTodoText: ''
+    }
+  },
+
+
+}).mount('#app')
