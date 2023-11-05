864b2583b4b37c971c98442e27f4c7437b7df666
Vahidone <v.ban.roma@gmail.com>
2023-11-05 12:57:04 +0100

    feat: add error message when try send empty input

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ createApp({
 
   },
 
+  
   methods: {
     getTodo() {
       axios.get(this.apiUrl)
@@ -27,6 +28,12 @@ createApp({
     
 
     addTodo() {
+      if (this.newTodoText.trim() === '') {
+        this.showError = true;
+        this.errorMessage = 'Non puoi lasciare il campo vuoto';
+        return;
+      }
+      
       const data = new FormData();
       data.append('newTodoText', this.newTodoText)
     
